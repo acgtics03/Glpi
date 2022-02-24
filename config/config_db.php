5869d063adefe8193dbc0971d0874a697582fe78
ACG TICS 03 <98350329+acgtics03@users.noreply.github.com>
2022-02-24 08:52:13 -0500

    CodigoBase
    Carga Codigo

---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost     = 'acg-soft.com';
+   public $dbuser     = 'acgsoft_glpi';
+   public $dbpassword = '5y52019adm';
+   public $dbdefault  = 'acgsoft_dbglpi';
+}
